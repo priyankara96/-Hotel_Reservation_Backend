2e796d518ffa9ee850969a5193754c6b1029cb2a
priyankara96 <chamidupriyankara.sl@gmail.com>
2022-05-15 21:36:51 +0530

    User management file updated

--- models/CommonSignup.js
+++ models/CommonSignup.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const CommonSignupSchema = new mongoose.Schema({
+const CommonSignupSchema = new mongoose.Schema ({
     name:{
         type: String,
         required: true,
